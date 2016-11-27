4be3773c56fb60fa4c11745e43325c694a24822d
HenriqueMorato <riquemorato@gmail.com>
2016-11-27 16:35:03 -0200

    * LoginPage.xaml.cs: Autenticação
    * Auth.iOS.csproj:
    * packages.config:
    * packages.config:
    * Auth.Droid.csproj:
    * Resource.designer.cs:

--- Auth/Pages/LoginPage.xaml.cs
+++ Auth/Pages/LoginPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,17 @@ namespace Auth
 	{		
 		Entry name;
 		Entry password;
+		private const string Url = "http://apiauthmob220161126081011.azurewebsites.net/api/usuarios";
+		private ObservableCollection<Users> _users;
+		
+		public class Users
+		{
+		    public int Id { get; set; }
+		    public string Nome { get; set; }
+		    public string NickNome { get; set; }
+		    public string Senha { get; set; }
+		    public bool Ativo { get; set; }
+		}
 		
 		public LoginPage()
 		{			
@@ -23,18 +35,32 @@ namespace Auth
 
 		void Handle_Clicked(object sender, System.EventArgs e)
 		{
-			var name = GetData();
-
-			
+			Login();
 		}
 		
-		public async Task<string> GetData ()
+		public async void Login ()
 		{
 			using (var client = new HttpClient())
 			{
-				var result = await client.GetAsync("http://apiauthmob220161126081011.azurewebsites.net/api/usuarios");
-				var myJson = result.Content.ReadAsStringAsync().Result;
-				return myJson;
+				var content = await client.GetStringAsync(Url);
+				var users = JsonConvert.DeserializeObject<List<Users>>(content);
+				
+				_users = new ObservableCollection<Users>(users);
+				
+				foreach(Users u in _users)
+				{
+					if(txtNome.Text == u.Nome)
+					{
+						if(txtSenha.Text == u.Senha)
+						{
+							await DisplayAlert("Ok", "Autenticou", "Ok");
+						}
+					}
+				}
+			
+				//var result = await client.GetAsync("http://apiauthmob220161126081011.azurewebsites.net/api/usuarios");
+				//var myJson = result.Content.ReadAsStringAsync().Result;
+				//return myJson;
 			}	
 		} 
 	}
